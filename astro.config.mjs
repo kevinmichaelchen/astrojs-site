a9537c0fcefd80ebaa28de057eb3d938f76270b5
Kevin Chen <kevinmichaelchen@gmail.com>
2022-09-03 23:40:45 -0400

    UnoCSS + Tailwind + Attributify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,40 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import Unocss from "unocss/astro";
+import presetWind from "@unocss/preset-wind";
+import presetWebFonts from "@unocss/preset-web-fonts";
+import presetAttributify from "@unocss/preset-attributify";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://example.com",
+  integrations: [
+    mdx(),
+    sitemap(),
+    Unocss({
+      // https://github.com/unocss/unocss#presets
+      presets: [
+        // Attributify Mode for UnoCSS.
+        // Useful for when the class="" string gets really long.
+        // https://github.com/unocss/unocss/tree/main/packages/preset-attributify
+        presetAttributify(),
+        // Tailwind / Windi CSS compact preset for UnoCSS.
+        // https://github.com/unocss/unocss/tree/main/packages/preset-wind
+        presetWind(),
+        // Web fonts support for UnoCSS.
+        presetWebFonts({
+          // default provider is 'google', but 'bunny' is also supported:
+          // https://fonts.bunny.net/ -- a privacy-friendly Google drop-in replacement
+          provider: "google",
+          fonts: {
+            // these will extend the default theme
+            sans: "DM Sans",
+            serif: "Source Serif Pro",
+            mono: ["Fira Code", "Fira Mono:400,700"],
+          },
+        }),
+      ],
+    }),
+  ],
 });
